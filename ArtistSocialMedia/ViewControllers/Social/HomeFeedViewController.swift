8855bcd2ed5497f4d28c80c3890f6bf54bfb8b2f
Casandra <casandragrullon@gmail.com>
2020-10-07 14:40:44 -0400

    added first view controller ui

---
+++ ArtistSocialMedia/ViewControllers/Social/HomeFeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtistSocialMedia
+//
+//  Created by casandra grullon on 10/7/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
