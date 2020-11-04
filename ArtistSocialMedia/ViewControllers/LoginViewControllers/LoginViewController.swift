d539757b1edc078200c43d38266b8e3914e016ce
Casandra <casandragrullon@gmail.com>
2020-11-04 18:26:43 -0500

    creating an account works properly

--- ArtistSocialMedia/ViewControllers/LoginViewControllers/LoginViewController.swift
+++ ArtistSocialMedia/ViewControllers/LoginViewControllers/LoginViewController.swift
@@ -39,6 +39,7 @@ class LoginViewController: UIViewController {
         configureTextFields()
         configureNavBar()
     }
+    //MARK:- Configure UI elements
     //textfields
     private func configureTextFields() {
         emailTextField.delegate = self
@@ -48,8 +49,16 @@ class LoginViewController: UIViewController {
     private func configureNavBar() {
         navigationController?.isNavigationBarHidden = true
     }
+    //MARK:- Login/ Sign Up methods
     //Login
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if accountState == .existingUser {
+            login()
+        } else {
+            signUp()
+        }
+    }
+    private func login() {
         guard let email = emailTextField.text, !email.isEmpty,
               let password = passwordTextField.text, !password.isEmpty else {
             DispatchQueue.main.async {
@@ -73,17 +82,54 @@ class LoginViewController: UIViewController {
             }
         }
     }
+    //Sign Up
+    private func signUp() {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty,
+              let confirm = confirmPasswordTextField.text, !confirm.isEmpty,
+              confirm == password else {
+            DispatchQueue.main.async {
+                self.showAlert(title: "Missing Fields", message: "Email and Password are required")
+            }
+            return
+        }
+        AuthenticationSession.shared.createNewUser(email: email, password: password) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.showAlert(title: "Sign Up Error", message: "Unable to sign up at this time error: \(error.localizedDescription)")
+                }
+            case .success(let authDataResult):
+                DispatchQueue.main.async {
+                    self?.createUser(authDataResult: authDataResult)
+                }
+            }
+        }
+    }
+    private func createUser(authDataResult: AuthDataResult) {
+        DatabaseService.shared.createAppUser(authDataResult: authDataResult) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                print("unable to create user on database error: \(error.localizedDescription)")
+            case .success:
+                DispatchQueue.main.async {
+                    self?.navigateToMainApp()
+                }
+            }
+        }
+    }
     private func navigateToMainApp() {
         UIViewController.showViewController(storyboardName: "Main", viewcontrollerID: "MainAppTabBar")
     }
     //TODO:- third party sign in methods
-    //Third Party Sign In
+    //MARK:-Third Party Sign In methods
     @IBAction func googleButtonPressed(_ sender: UIButton) {
     }
     @IBAction func facebookButtonPressed(_ sender: UIButton) {
     }
     @IBAction func twitterButtonPressed(_ sender: UIButton) {
     }
+    //MARK:- Toggle account state methods
     //Sign Up
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
         accountState = accountState == .existingUser ? .newUser : .existingUser
